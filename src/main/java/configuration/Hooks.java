4943a5934d169842c22fd30c8263854c6b3a420f
Matheus Machado Luiz - Terceiro <vn0k8ao@BR.wal-mart.com>
2020-10-21 10:40:25 -0300

    adição do hooks para o snf

--- src/main/java/configuration/Hooks.java
+++ src/main/java/configuration/Hooks.java
@@ -2,6 +2,7 @@
 package configuration;
 
 import commons.BaseTest;
+import commons.CredentialsUsers;
 import io.cucumber.core.api.Scenario;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
@@ -15,9 +16,22 @@ public class Hooks extends BaseTest {
 		initializeWebApplication(Web.CHROME);
 	}
 
+	@Before(value = "@webSNF")
+	public void beforeScenarioWebSNF() {
+		System.out.println("Teste execução Hooks Web");
+		initializeWebApplication(Web.CHROME);
+		setUserApplicationUsers(CredentialsUsers.USER_WEB_SNF_QA);
+	}
 
 	@After(value = "@web")
 	public void closeDriver(Scenario scenario) {
+		generateEvidence(scenario);
+		evidenceList.clear();
+		closeWeb();
+	}
+
+	@After(value = "@webSNF")
+	public void closeDriverSNF(Scenario scenario) {
 		generateEvidence(scenario);
 		evidenceList.clear();
 		closeWeb();
